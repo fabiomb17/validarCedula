2bd66d1fb744d0a873e70353f32c4e997c380ccf
Fabio Muñoz <fabiomb17@gmail.com>
2022-04-18 22:45:01 -0400

    validador de cedulas Dominicanas

---
+++ validarCedula.js
@@ -0,0 +1,51 @@
+function valida(cedula){
+    
+    cedula = document.getElementById('cedula').value;
+    var cantidad = cedula.length;
+    
+    if(cantidad == 11){
+                
+        c = cedula.split('');
+        //  [2,2,3,0,0,2,2,4,3,7,9]
+        v = [1,2,1,2,1,2,1,2,1,2]
+                
+        var result = 0 ; 
+        var ar,up,oc;
+              
+        for (i=0;i <10;i++){  
+            up =c[i] * v[i];
+            ab = up;
+            
+            if ( ab >= 10 ) {
+                oc = ab.toString()
+                    .split('')
+                    .map(x => parseInt(x) )
+                    .reduce( (x, y) => x + y);
+            }else {
+                oc = ab;
+            }
+            
+            result = parseFloat(result) + parseFloat(oc);
+        }   
+            
+        dp = result;
+        ac = dp.toString().split('')[0] + '0';
+        ac = parseInt(ac);
+        uj = (ac / 10) * 10;
+        
+            if (uj < dp ) {
+                dp = (uj + 10) - dp; 
+            }   
+
+            if (c[10] == dp) { 
+                
+                document.getElementById('result').innerHTML = '<p> Cedula Correcta </p>';
+            } else {
+                
+                document.getElementById('result').innerHTML = '<p> Cedula Incorrecta </p>';    
+            }
+            
+        }else{
+            document.getElementById('result').innerHTML = '<p>Ingrese una cedula valida</p>';
+        }
+   }       
